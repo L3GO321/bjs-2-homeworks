35336e2bdf3de790f088cd73d772fc8443ec73c6
L3GO321 <googlik701@gmail.com>
2022-02-01 16:57:56 +0300

    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,65 @@
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let roots = [];
+  let D = b ** 2 - 4 * a * c;
+
+  switch(true) {
+    case D < 0:
+      return roots;
+      break;
+    case D === 0:
+      roots.push(-b / (2 * a));
+      return roots;
+      break;
+    case D > 0:
+      roots.push((-b + Math.sqrt(D)) / (2 * a), (-b - Math.sqrt(D)) / (2 * a));
+      return roots;
+  }
+  
+  //Второй вариант условия
+  // if (D < 0) {
+  //   return roots;
+  // } else if (D === 0) {
+  //   roots.push(-b / (2 * a));
+  //   return roots;
+  // } else if (D > 0) {
+  //   roots.push((-b + Math.sqrt(D)) / (2 * a), (-b - Math.sqrt(D)) / (2 * a));
+  //   return roots;
+  // }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+  let totalAmount = 0; 
 
-  // код для задачи №2 писать здесь
+  if (Number.isNaN(Number(percent))) {
+    return (`Параметр <Процентная ставка> содержит неправильное значение <${percent}>`);
+  }
+
+  if (Number.isNaN(Number(contribution))) {
+    return (`Параметр <Начальный взнос> содержит неправильное значение <${contribution}>`);
+  } 
+
+  if (Number.isNaN(Number(amount))) {
+    return (`Параметр <Общая стоимость> содержит неправильное значение <${amount}>`);
+  }
+
+  percent = Number(percent);
+  contribution = Number(contribution);
+  amount = Number(amount);
+
+  const creditBody = amount - contribution;
+  const interestRate = percent / 100 / 12;
+
+  const currentDate = new Date();
+
+  let months = (date.getFullYear() - currentDate.getFullYear()) * 12 - (currentDate.getMonth() + 1) + date.getMonth();
+
+  if (date.getDate() >= currentDate.getDate()) {
+    months++;
+  }
+
+  totalAmount = ((creditBody * (interestRate + (interestRate / (((1 + interestRate) ** months) - 1)))* months)).toFixed(2);
 
   return totalAmount;
 }
