d89354a4f08ec8295337470747024e6de754362c
L3GO321 <googlik701@gmail.com>
2022-02-27 12:45:15 +0300

    Initial commit

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -93,7 +93,7 @@ class Library {
             }
         }
 
-        if (givenBookIndex) {
+        if (givenBookIndex !== -1) {
             this.books.splice(givenBookIndex, 1);
         }
 
@@ -102,5 +102,59 @@ class Library {
 }
 
 //Hometask 3
+class Student {
+    constructor(name, gender, age) {
+      this.name = name;
+      this.gender = gender;
+      this.age = age;
+      this.marks = {};
+    }
 
+    setSubject(subject) {
+        this.marks[subject] = [];
+      }
+
+    addMark(mark, subject) {
+      if (mark < 1 || mark > 5) {
+          return 'Ошибка, оценка должна быть числом от 1 до 5';
+      }
+        
+      if (this.marks.hasOwnProperty(subject)) {
+        this.marks[subject].push(mark);
+      } else {
+        this.marks[subject] = [mark];
+      }
+    }
+
+    getAverageBySubject(subject) {
+        if (!this.marks.hasOwnProperty(subject)) {
+            return 'Несуществующий предмет';
+        } 
+
+        let sum = this.marks[subject].reduce((sum, mark) => sum + mark);
+        return `Средний балл по предмету ${subject} ${sum / this.marks[subject].length}`;
+    }
+
+    getAverage() {
+        let sumOfMarks = 0;
+        let numberOfMarks = 0;
+
+        for (let key in this.marks) {
+            sumOfMarks += this.marks[key].reduce((sum, mark) => sum + mark);
+            numberOfMarks += this.marks[key].length;
+        } 
+
+        if (numberOfMarks) {
+            return `Средний балл по всем предметам ${sumOfMarks / numberOfMarks}`;
+        }
+
+        return 'Журнал оценок пуст';
+    }
+
+    exclude(reason) {
+      delete this.marks;
+  
+      this.excluded = reason;
+    }
+  }
 
